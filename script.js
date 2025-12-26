11b24e93641d246ee00ab6af45e5f6f6fbe69a5b
prisca1226 <dellaprisca007@gmail.com>
2025-12-26 18:53:08 +0800

    Refactor text handling for typewriter effect

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const text = 'i just want to tell u. i dont like u anymore.
+const textLines = [
+  "i just want to tell u. i dont like u anymore.
 my crush era was end, cuz theres no chance for us to be us , right?
 so, i dont want u anymore, i dont want to bother ur world.
 i will let u go, i am also ready if one day
 i see u with the woman of ur choice.
 thanks for being the reason i smile and
-excited about going campus:)';
+excited about going campus"
+  ];
 
+let currentLine = 0;
 let i = 0;
 const speed = 100; // kecepatan ketik (ms)
 
 function typeWriter() {
-  if (i < text.length) {
-    document.getElementById("typedText").innerHTML += text.charAt(i);
+  if (currentLine < textLines.length) {
+    if ( i < textLines[currentLine].length)
+    {
+    document.getElementById("typedText").innerHTML += textLines[currentLine].charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+    } else {
+      document.getElementById("typedText").innerHTML += "<br><br>";
+      currentLine++;
+      i = 0;
+      setTimeout(typeWriter, speed);
   }
 }
-
-window.onload = typeWriter;
+}
+typeWriter();
