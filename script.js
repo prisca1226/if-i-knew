a3d0bba23ae260158c5f077c719f49dbb7f58edf
prisca1226 <dellaprisca007@gmail.com>
2025-12-26 18:38:02 +0800

    Fix text formatting and punctuation in script.js
    Corrected text formatting and punctuation in the message.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const text = `i just want to tell u. i dont like u anymore.
-my crush era was end, cuz theres no chance for us to be "us" , right?
+const text = 'i just want to tell u. i dont like u anymore.
+my crush era was end, cuz theres no chance for us to be us , right?
 so, i dont want u anymore, i dont want to bother ur world.
-
-i'll let u go, i am also ready if one day
+i will let u go, i am also ready if one day
 i see u with the woman of ur choice.
-
 thanks for being the reason i smile and
-excited about going campus:)`;
+excited about going campus:)';
 
 let i = 0;
 const speed = 100; // kecepatan ketik (ms)
